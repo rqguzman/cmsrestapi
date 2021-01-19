ad5282cbd26cbb9f038d78a4ea8a263507534d64
rqguzman <rafael.guzman@outlook.com>
2021-01-19 13:42:19 -0300

    :tada: Project created

---
+++ src/main/java/com/guzmanr/restapi/CmsrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.guzmanr.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsrestapiApplication.class, args);
+	}
+
+}
